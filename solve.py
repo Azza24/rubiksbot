0f00ee103a3606fec6b8ac1688bb5024eacbaa22
Azza24 <marije.heeren@gmail.com>
2020-03-28 22:19:37 +0100

    Wrote alg for corners

--- solve.py
+++ solve.py
@@ -96,16 +96,110 @@ while solvedEdge == 0:
     if solvedtmp == 1:
         solvedEdge = 1
 
-# Solving Corners
+def printCorners(argument):
+    switcher = {
+        1: "U, L U L F' L' F U' L' R' U' R' F R F' U R U'",  # Orange Yellow (1, 0)
+        2: "Switch",                                    # Yellow Red (2, 3)
+        3: "L, U', F, U, Switch, U', F', U, L'",        # Red Yellow (3, 2)
+        4: "R2, U', R2, Switch, R2, U, R2",             # Yellow Blue (4, 5)
+        5: "R', B, L, R, Switch, R', L', B', R",        # Blue Yellow (5, 4)
+        6: "R2, R, R2, Switch, R2, R' R2",              # Yellow Green (6, 7)
+        7: "U', F', U, L', Switch, L, U', F, U",        # Green Yellow (7, 6)
+        8: "U2, R', U2, Switch, U2, R U2",              # Blue Orange (8, 9)
+        9: "U, B, U', Switch, U, B', U'",               # Orange Blue (9, 8)
+        10: "U', F' U, Switch, U', F, U",               # Orange Green (10, 11)
+        11: "U2,  R, U2, Switch, U2, R', U2",           # Green Orange (11, 10)
+        12: "L', Switch, L",                            # Green Red (12, 13)
+        13: "U', F', U, Switch, U' F",                  # Red Green (13, 12)
+        14: "U, B' U', Switch, U, B, U'",               # Red Blue (14, 15)
+        15: "L, Switch, L'",                            # Blue Red (15, 14)
+        16: "D2, L2, Switch, L2, D2",                   # White Orange (16, 17)
+        17: "D', U', F, U, L', Switch, L, U', F', U, D",  # Orange White (17, 16)
+        18: "L2, Switch, L2",                           # White Red (18, 19)
+        19: "L', U', F, U, Switch, U', F', U, L",       # Red White (19, 18)
+        20: "D, L2, Switch, L2, D'",                    # White Blue (20, 21)
+        21: "U, B' U', L, Switch, L', U, B, U'",        # Blue White (21, 20)
+        22: "D', L2, Switch, L2, D",                    # White Green (22, 23)
+        23: "U', F, U, L', Switch, L, U', F', U"        # Green White (23, 22)
+    }
+    print(switcher.get(argument))
 
 solvedCorner = 0
 
 while solvedCorner == 0:
-   switchPos = 0
-   found = 0
+    if corner[0] == 0:
+        for x in range(24):
+            if cor[x] != corner[x]:
+                printCorners(x)
+                tmp1 = corner[0]
+                tmp2 = corner[1]
+                tmp3 = corner[2]
+                corner[0] = corner[x]
+                corner[1] = corner[x+1]
+                corner[2] = corner[x+2]
+                corner[x] = tmp1
+                corner[x+1] = tmp2
+                corner[x+2] = tmp3
+                break
+    elif corner[0] == 1:
+        for x in range(1, 23):
+            if cor[x] != corner[x]: # and another argument
+                print("some shit needs to go here")
+                break
+    elif corner[0] == 2:
+        for x in range(2, 23):
+            if cor[x] != corner[x]: # and another argument
+                print("Some other shit goes here")
+                break
 
-    while found == 0:  # look for position to be switched with
-        if edge[0] == cor[switchpos]:
+    switchPos = 0
+    found = 0
+
+    while found == 0:
+        if corner[0] == cor[switchPos]:
             found = 1
         else:
-            switchpos += 2
+            switchPos += 3
+
+    printCorners(switchPos)
+    if switchPos == 3 ^ 6 ^ 9 ^ 12 ^ 15 ^ 18 ^ 21:
+        tmp1 = corner[0]
+        tmp2 = corner[1]
+        tmp3 = corner[2]
+        corner[0] = corner[x]
+        corner[1] = corner[x + 1]
+        corner[2] = corner[x + 2]
+        corner[x] = tmp1
+        corner[x + 1] = tmp2
+        corner[x + 2] = tmp3
+    elif switchPos == 4 ^ 7 ^ 10 ^ 13 ^ 16 ^ 19 ^ 22:
+        tmp1 = corner[0]
+        tmp2 = corner[1]
+        tmp3 = corner[2]
+        corner[0] = corner[x + 2]
+        corner[1] = corner[x]
+        corner[2] = corner[x + 1]
+        corner[x] = tmp3
+        corner[x + 1] = tmp1
+        corner[x + 2] = tmp2
+    else:
+        tmp1 = corner[0]
+        tmp2 = corner[1]
+        tmp3 = corner[2]
+        corner[0] = corner[x + 1]
+        corner[1] = corner[x + 2]
+        corner[2] = corner[x]
+        corner[x] = tmp2
+        corner[x + 1] = tmp3
+        corner[x + 2] = tmp1
+
+    solvedtmp = 1
+    for x in range(24):
+        if cor[x] != corner [x]:
+            solvedtmp = 0
+        if solvedtmp == 1:
+            solvedCorner = 1
+
+    print(corner)
+
+
